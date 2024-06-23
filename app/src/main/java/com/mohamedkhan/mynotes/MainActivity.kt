c53fe446a32d4f65a998fd2c93692f0684539c35
Khan <mohamedfayaskhan.a@gmail.com>
2024-06-23 17:21:37 +0530

    Recycler UI finished

--- app/src/main/java/com/mohamedkhan/mynotes/MainActivity.kt
+++ app/src/main/java/com/mohamedkhan/mynotes/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
